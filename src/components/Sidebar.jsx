b9b89550df47affbce46f9d2f29f3d4afb92eddb
unknown <flofey@yandex.ru>
2024-06-07 21:37:17 +0500

    sibebar fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,11 +7,11 @@ export default function Sidebar({setOpenBar}) {
         <motion.div initial={{left: -100}} animate={{left: 0}} className="h-screen w-[70vw] text-white bg-[#111] flex-col flex p-5 fixed z-50 text-2xl">
             <div className={"py-5 pl-5 mt-10 flex flex-col"}>
                 <Link to="/" className={"border-b-[1px] active:text-orange focus:text-orange border-white p-3"}
-                      onClick={() => setOpenBar(false)}>Главная</Link>
+                      onClick={() => setOpenBar(false)}>Главное</Link>
                 <Link to="/services" className={"border-b-[1px] active:text-orange focus:text-orange border-white p-3"}
                       onClick={() => setOpenBar(false)}>Услуги</Link>
                 <Link to="/connection" className={"border-b-[1px] active:text-orange focus:text-orange border-white p-3"}
-                      onClick={() => setOpenBar(false)}>Связи</Link>
+                      onClick={() => setOpenBar(false)}>Связь</Link>
             </div>
         </motion.div>
     </div>
